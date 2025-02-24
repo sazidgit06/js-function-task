c4364a4a23a7b8c6e5fd9f3aa5b7af4dd81a429c
Sazid <sazidtec06@gmail.com>
2025-02-24 09:36:36 +0600

    task1 complete

---
+++ task1.js
@@ -0,0 +1,11 @@
+
+/*
+    Task-1
+    Take four parameters. Multiply the four numbers and then return the result
+*/
+
+function mul(a,b,c,d){
+    return a*b*c*d;
+}
+const res = mul(1,2,3,4);
+console.log('Result is: ',res);
